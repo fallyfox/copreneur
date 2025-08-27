20e7788de5d4e75933721026fe26d55dde0e2eeb
Joseph Ogbu <fallyfox@gmail.com>
2025-08-27 15:58:04 +0100

    added app description

--- app/about.js
+++ app/about.js
@@ -4,6 +4,7 @@ export default function About () {
     return (
         <View>
             <Text>About copreneur</Text>
+            <Text>An app for developer to collaborate with entrepreneurs</Text>
         </View>
     )
 }
