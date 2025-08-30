64aef547e24c298bf217fd540a53ba684c66c599
Joseph Ogbu <fallyfox@gmail.com>
2025-08-30 16:41:24 +0100

    flex box lesson

--- app/_layout.js
+++ app/_layout.js
@@ -1,26 +1,30 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  const session = false;
+  
   return (
     <Stack>
+      {session === true ?
       <Stack.Screen
       name="(tabs)"
       options={{
         title: "Home",
         headerShown: false
       }}/>
+      :
+      <Stack.Screen
+      name="signup"
+      options={{
+        title: "Create a new account",
+        headerShown: false,
+      }}/>
+      }
       
       <Stack.Screen
       name="index"
       options={{
         title: "Welcome",
-        headerShown: false,
-      }}/>
-      
-      <Stack.Screen
-      name="signup"
-      options={{
-        title: "Create a new account",
         headerShown: false,
       }}/>
       
--- app/signup.js
+++ app/signup.js
@@ -1,9 +1,56 @@
-import { Text, View } from "react-native";
+import { Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { colors } from "../theme/colors";
 
 export default function Signup () {
     return (
-        <View>
-            <Text>Sign up screen</Text>
+        <View style={styles.wrapper}>
+            {/* header group */}
+            <View style={styles.header}>
+                <Text style={styles.brandName}>Copreneur</Text>
+                <Text style={styles.brandDesc}>Where entrepreneurs collaborate with developers</Text>
+            </View>
+
+            {/* body group */}
+            <View>
+                <Text>Create account</Text>
+                <TouchableOpacity>
+                    <Image
+                    style={{
+                        width: 36,
+                        height: 36,
+                    }}
+                    source={require("../assets/images/google.png")}/>
+                    <Text>Google</Text>
+                </TouchableOpacity>
+            </View>
+
+            {/* bottom group */}
+            <View></View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+   wrapper: {
+    flex: 1,
+    backgroundColor: colors.brown200,
+    paddingTop: StatusBar.currentHeight,
+    paddingHorizontal: 16
+   }, 
+   header: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: 8
+   },
+   brandName: {
+    fontSize: 46,
+    fontWeight: "bold",
+    color: colors.brown400
+   },
+   brandDesc: {
+    fontWeight: "semibold",
+    color: colors.brown400
+   }
+});
+
