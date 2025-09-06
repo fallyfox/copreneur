6000dc0c6f40b6ff6cd8bb66f6c84ff54c714307
Joseph Ogbu <fallyfox@gmail.com>
2025-09-06 16:11:00 +0100

    schema for cv screen

--- app/(tabs)/cv.js
+++ app/(tabs)/cv.js
@@ -1,19 +1,44 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text } from "react-native";
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function CV () {
     return (
         <SafeAreaProvider>
             <SafeAreaView>
-                <Text>CV</Text>
-                <Text>Welcome to copreneur</Text>
-                <Link 
-                href="/signup"
-                style={{
-                    fontWeight: "bold",
-                    color: "brown"
-                }}>Create a new account</Link>
+                <ScrollView>
+                    {/* header */}
+                    <View style={styles.header}>
+                        <Text style={styles.title}>Joseph Ogbu</Text>
+
+                        <View style={styles.subHeader}>
+                            <Text style={styles.subTitle}>React Native Developer</Text>
+                            <View style={styles.progress}>
+                                <View style={styles.unitProgress}>
+                                    <View style={styles.progressHeading}>
+                                        <Entypo name="time-slot" size={24} color="black" />
+                                        <Text style={styles.progressTitle}>Years</Text>
+                                    </View>
+                                    <Text style={styles.progressValue}>3</Text>
+                                </View>
+                                <View style={styles.unitProgress}>
+                                    <View style={styles.progressHeading}>
+                                        <Feather name="command" size={24} color="black" />
+                                        <Text style={styles.progressTitle}>Level</Text>
+                                    </View>
+                                    <Text style={styles.progressValue}>Pro</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+
+                    {/* skills */}
+
+                    {/* projects */}
+
+                    {/* contributions */}
+                </ScrollView>
             </SafeAreaView>
         </SafeAreaProvider>
     )
